6d88d4e3c9bba382e000e382bed00043c0d2a194
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-11-21 12:24:01 -0500

    On to design!

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 // console.log('Index.js runs')
-const buildMarker= require('./marker')
+const buildMarker = require('./marker');
 
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 // var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-mapboxgl.accessToken = "pk.eyJ1Ijoic3V5YXNoMTkiLCJhIjoiY2phOXRyMDlyMGx6ZjMybzdzMnQ1ZHJzMiJ9.IbKhkyOj2XFb2PSOlNYr8w";
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3V5YXNoMTkiLCJhIjoiY2phOXRyMDlyMGx6ZjMybzdzMnQ1ZHJzMiJ9.IbKhkyOj2XFb2PSOlNYr8w';
 
 const map = new mapboxgl.Map({
-  container: "map",
+  container: 'map',
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
   zoom: 12, // starting zoom
-  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
 });
 
 // var newDom = document.createElement('div');
 // var map = document.getElementById('map')
 // console.log(map)
-// new mapboxgl.Marker().setLngLat([-74.009, 40.705]).addTo(map); 
+// new mapboxgl.Marker().setLngLat([-74.009, 40.705]).addTo(map);
 
 // var marker = new mapboxgl.Marker()
 //   .setLngLat([-74.009, 40.705])
 //   .addTo(map);
 
-const markerDomEl = document.createElement("div");
-markerDomEl.style.width = "32px";
-markerDomEl.style.height = "39px";
-markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+// const markerDomEl = document.createElement("div");
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+buildMarker(hotels, [74.009, 40.705]);
--- src/marker.js
+++ src/marker.js
@@ -1,15 +1,17 @@
-
-const mapbox = require("mapbox-gl");
+const mapbox = require('mapbox-gl');
 
 const iconURLs = {
-    hotels: "http://i.imgur.com/D9574Cu.png",
-    restaurants: "http://i.imgur.com/cqR6pUI.png",
-    activities: "http://i.imgur.com/WbMOfMl.png"
+  hotels: 'http://i.imgur.com/D9574Cu.png',
+  restaurants: 'http://i.imgur.com/cqR6pUI.png',
+  activities: 'http://i.imgur.com/WbMOfMl.png',
 };
 
 const buildMarker = (type, coords) => {
-    // Your Code Here
-    
+  const markerDomEl = document.createElement('div');
+  markerDomEl.style.width = '32px';
+  markerDomEl.style.height = '39px';
+  markerDomEl.style.backgroundImage = iconURLs[type];
+  return new mapboxgl.Marker(markerDomEl).setLngLat(coords).addTo(map);
 };
 
 module.exports = buildMarker;
