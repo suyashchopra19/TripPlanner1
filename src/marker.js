b5b900f69ce4afbce45b3344f1bc571d819603f9
Suyash Chopra <suyashchopra19@gmail.com>
2017-11-21 11:50:59 -0500

    building markers

---
+++ src/marker.js
@@ -0,0 +1,15 @@
+
+const mapbox = require("mapbox-gl");
+
+const iconURLs = {
+    hotels: "http://i.imgur.com/D9574Cu.png",
+    restaurants: "http://i.imgur.com/cqR6pUI.png",
+    activities: "http://i.imgur.com/WbMOfMl.png"
+};
+
+const buildMarker = (type, coords) => {
+    // Your Code Here
+    
+};
+
+module.exports = buildMarker;
